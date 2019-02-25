7e9f4ba37379361f6c1fcc316520bc5ca360f476
argonavt11 <argonavt.al@yandex.ru>
2019-02-25 12:22:31 +0300

    avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Avatar from "./components/Avatar/Avatar";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Avatar src="https://www.backpackerguide.nz/wp-content/uploads/2017/10/avatar.jpg" />
       </div>
     );
   }
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./style";
+
+export interface IAvatarProps {
+  src: string;
+}
+
+const Avatar = (props: IAvatarProps) => <S.Wrapper>1</S.Wrapper>;
+
+export default Avatar;
