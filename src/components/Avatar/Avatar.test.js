0773e08dd7028e0a0bc3d5686a0d87cfa881dfb5
argonavt11 <argonavt.al@yandex.ru>
2019-02-27 16:44:46 +0300

    alt and src

--- src/components/Avatar/Avatar.test.js
+++ src/components/Avatar/Avatar.test.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import Avatar from "./Avatar";
 
-it("render with only src", () => {
-  const block = shallow(<Avatar src={"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"} />);
-  const img = block.find("img")
-  console.log(img.html());
-  // expect(img).toEqual(false);
-})
+it("render horizontal image with only src and alt", async () => {
+  const urlImg =
+    "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png";
+  const altImg = "picher";
+  const block = shallow(<Avatar src={urlImg} alt={altImg}/>);
+  const img = block.find("img");
+  expect(img.exists()).toEqual(true);
+  expect(img.prop("src")).toEqual(urlImg);
+  expect(img.prop("alt")).toEqual(altImg);
+  await block.instance().renderImg();
+  console.log(img.html())
+});
