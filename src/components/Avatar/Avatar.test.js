05d019069237e943789872b39f92629e6dac3b43
argonavt11 <argonavt.al@yandex.ru>
2019-02-27 15:33:41 +0300

    circle and test

--- src/components/Avatar/Avatar.test.js
+++ src/components/Avatar/Avatar.test.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Avatar from "./Avatar";
 
-it("render", () => {
+it("render with only src", () => {
   const block = shallow(<Avatar src={"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"} />);
-  expect(block.html()).toEqual(false);
+  const img = block.find("img")
+  console.log(img.html());
+  // expect(img).toEqual(false);
 })
