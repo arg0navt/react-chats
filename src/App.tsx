1b7659992543845a786614d2386b54b8f82c8ac0
argonavt11 <argonavt.al@yandex.ru>
2019-02-28 14:30:36 +0300

    message style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Avatar from "./components/Avatar/Avatar";
+import Message from "./components/Message/Message";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Avatar 
-          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" 
+        <Avatar
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
           alt=""
           online={true}
+        />
+        <Message
+          message={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+          date={"14:35 22.02.2019"}
         />
       </div>
     );
--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./Message.module.scss";
 
-export default class Message extends React.Component {
+export interface IMessageProps {
+  float: string;
+  message?: string;
+  date?: string;
+}
+
+export default class Message extends React.Component<IMessageProps> {
+  static defaultProps = {
+    float: "left",
+    message: "",
+    date: ""
+  };
   render() {
-    return <div />
+    return (
+      <div className={`${styles.block} ${styles["float-" + this.props.float]}`}>
+        {this.props.message ? (
+          <p className={styles.messageText}>{this.props.message}</p>
+        ) : null}
+        {this.props.date ? (
+          <span className={styles.date}>{this.props.date}</span>
+        ) : null}
+      </div>
+    );
   }
 }
