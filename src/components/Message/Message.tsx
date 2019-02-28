9c04ad8ad52d01521ca28b5203cde4b65d45c9da
argonavt11 <argonavt.al@yandex.ru>
2019-02-28 15:07:50 +0300

    left and right triangle

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -9,20 +9,24 @@ export interface IMessageProps {
 
 export default class Message extends React.Component<IMessageProps> {
   static defaultProps = {
-    float: "left",
+    float: "right",
     message: "",
     date: ""
   };
   render() {
     return (
-      <div className={`${styles.block} ${styles["float-" + this.props.float]}`}>
-        {this.props.message ? (
-          <p className={styles.messageText}>{this.props.message}</p>
-        ) : null}
-        {this.props.date ? (
-          <span className={styles.date}>{this.props.date}</span>
-        ) : null}
-      </div>
+      <>
+        <div
+          className={`${styles.block} ${styles["float-" + this.props.float]}`}
+        >
+          {this.props.message ? (
+            <p className={styles.messageText}>{this.props.message}</p>
+          ) : null}
+          {this.props.date ? (
+            <span className={styles.date}>{this.props.date}</span>
+          ) : null}
+        </div>
+      </>
     );
   }
 }
